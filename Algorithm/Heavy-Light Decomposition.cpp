730423cbec5e661c622f2759e70193cd9d0d9daa
SeongHyeon Cho <67233988+st42597@users.noreply.github.com>
2021-10-25 09:07:39 +0900

    Update Heavy-Light Decomposition.cpp

--- Algorithm/Heavy-Light Decomposition.cpp
+++ Algorithm/Heavy-Light Decomposition.cpp
@@ -3,38 +3,36 @@ using namespace std;
 typedef pair<int, int> pii;
 const int N = 100001;
 int sz[N], d[N], p[N], top[N], in[N], out[N];
-vector<int> adj[N], c[N];
+vector<int> arr, adj[N], c[N];
 
-struct Input{
-    int u, v, w;
-};
-vector<Input> arr;
 int seg_size;
 struct Seg{
     int tree[N * 4];
     int x, y, val;
-    void set_updaterange(int _x, int _val){
+    int st_update(int _x, int _val){
         x = _x; val = _val;
+        return update();
     }
     int update(int s = 1, int e = seg_size, int pos = 1){
         if(e < x || x < s) return tree[pos];
         if(s == e) return tree[pos] = val;
-        return tree[pos] = max(update(s, (s + e) / 2, pos * 2), update((s + e) / 2 + 1, e, pos * 2 + 1));
+        return tree[pos] = update(s, (s + e) / 2, pos * 2) ^ update((s + e) / 2 + 1, e, pos * 2 + 1);
     }
-    void set_queryrange(int _x, int _y){
+    int st_query(int _x, int _y){
         x = _x; y = _y;
+        return query();
     }
     int query(int s = 1, int e = seg_size, int pos = 1){
         if(e < x || y < s) return 0;
         if(x <= s && e <= y) return tree[pos];
-        return max(query(s, (s + e) / 2, pos * 2), query((s + e) / 2 + 1, e, pos * 2 + 1));
+        return query(s, (s + e) / 2, pos * 2) ^ query((s + e) / 2 + 1, e, pos * 2 + 1);
     }
 } seg;
 bool isVisited[N];
 void dfs(int v){
+    isVisited[v] = true;
     for(int next : adj[v]){
         if(isVisited[next]) continue;
-        isVisited[next] = true;
         c[v].push_back(next);
         dfs(next);
     }
@@ -63,41 +61,33 @@ int query(int a, int b){
     while(top[a] != top[b]){
         if(d[top[a]] < d[top[b]]) swap(a, b);
         int st = top[a];
-        seg.set_queryrange(in[st], in[a]);
-        ret = max(ret, seg.query());
+        ret ^= seg.st_query(in[st], in[a]);
         a = p[st];
     }
     if(d[a] > d[b]) swap(a, b);
-    seg.set_queryrange(in[a] + 1, in[b]);
-    ret = max(ret, seg.query());
+    ret ^= seg.st_query(in[a], in[b]);
     return ret;
 }
 int main(){
     ios::sync_with_stdio(false); cin.tie(0);
-    int n; cin >> n; seg_size = n;
+    int n, q; cin >> n >> q; seg_size = n;
+    for(int i = 0; i < n; i++){
+        int tmp; cin >> tmp;
+        arr.push_back(tmp);
+    }
     for(int i = 0; i < n - 1; i++){
-        int u, v, w; cin >> u >> v >> w;
+        int u, v; cin >> u >> v;
         adj[u].push_back(v);
         adj[v].push_back(u);
-        arr.push_back({u, v, w});
     }
-    isVisited[1] = true;
-    dfs(1); dfs1(1); dfs2(1);
-    for(Input tmp : arr){
-        seg.set_updaterange((d[tmp.u] > d[tmp.v] ? in[tmp.u] : in[tmp.v]), tmp.w);
-        seg.update();
+    dfs(1); dfs1(1); dfs2(1); 
+    for(int i = 0; i < n; i++){
+        seg.st_update(in[i + 1], arr[i]);
     }
-    int q; cin >> q;
     while(q--){
         int op, a, b; cin >> op >> a >> b;
-        if(op == 1){
-            a--;
-            int vt = (d[arr[a].u] > d[arr[a].v] ? arr[a].u : arr[a].v);
-            seg.set_updaterange(in[vt], b);
-            seg.update();
-        }else{
-            cout << query(a, b) << "\n";
-        }
+        if(op == 1) seg.st_update(in[a], b);
+        else cout << query(a, b) << "\n";
     }
     return 0;
 }
